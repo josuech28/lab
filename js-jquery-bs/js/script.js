b993f357351bf7bbf9ded0d8c4eb638a221fe89d
Josue Chinchilla <josuech28@gmail.com>
2017-04-01 21:56:47 -0600

    Id selector success Add list of buttons

--- js-jquery-bs/js/script.js
+++ js-jquery-bs/js/script.js
@@ -1,3 +1,59 @@
+showResults = function (texto, estilo) {
+    var output = $("#mostrarResultado");
+    if (output) {
+        output.attr("class", estilo);
+        output.text(texto);
+    }
+}
+// constructor func
+function User(name, dateP) {
+    var age;
+    // private method
+    var calculateAge = function () {
+        var date = new Date().getFullYear();
+        date = parseInt(date);
+        var yearBirth = dateP;
+        yearBirth = parseInt(yearBirth);
+
+        if (date <= yearBirth) {
+            age = 0; //error
+        }
+        else {
+            age = (date - yearBirth);
+            console.log(age);
+        }
+    };
+
+    this._name = name;
+    this._yearBirth = dateP;
+    // public method
+    this.toShow = function () {
+        calculateAge();
+        if (age != 0) {
+            var result =
+            "Hola, mi nombre es " + this._name +
+            " y tengo " + age + " años."
+            showResults(result,'add-success');
+        } else {
+            showResults("Error, birth year grather than current day",'add-error');
+        }
+    };
+}
+
+// Actions 
 $("#btn_calcular_edad").click(function () {
-    alert('Click!');
+    var name = $("#name").val();
+    var age = $("#fecha_nacimiento").val();;
+    var res = parseInt(age.substring(0, 4));
+    var newUser = new User(name,res);
+    newUser.toShow();
+    
 });
+
+$("#btn_id_selector").click(function () {
+    var text = $("#thing1").text();
+    showResults(text,"selector-id"); 
+});
+
+
+
